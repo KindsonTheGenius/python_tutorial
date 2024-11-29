cf24f9a49b67664cf1f46174661c342c5dfc5fee
SittingCrowd <zino.areh@yahoo.com>
2024-11-29 11:29:51 -0600

    update changes

--- zino_space/assignment1.py
+++ zino_space/assignment1.py
@@ -1,14 +1,14 @@
 # Write a simple python program that displays a text
 
 def say_hello():
-    print("Hello World")
+    print("Hello World")  # This is an output statement
     # Write two more output statements
-    print("-------------")
-    print("*************")
+    print("-------------")  # output statement to underline text
+    print("*************")  # output statement to underline text
 
-    name = "Zino Areh"
+    name = "Zino Areh"  # variable assignment
 
-    print(f"My name is {name}")
+    print(f"My name is {name}")  # String interpolation by using f before the string
 
 
 if __name__ == "__main__":
