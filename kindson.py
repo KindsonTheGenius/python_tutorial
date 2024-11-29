4c08b0b377f78365479979cd5e9d21fc6f020bfb
KindsonTheGenius <Smart24*>
2024-11-29 17:43:00 +0100

    Updated my details

---
+++ kindson.py
@@ -0,0 +1,8 @@
+'''
+    Name: Kindson Munonye
+    Course: Python Training
+    Duration: 3 Months
+    Location: Hungary
+'''
+
+# ------------------------------------------------------------------------------
