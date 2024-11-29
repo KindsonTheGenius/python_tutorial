56cd71873fd3e2eaa1f76854f5426af26a9d5618
KindsonTheGenius <Smart24*>
2024-11-29 18:09:11 +0100

    Updated my details

---
+++ kindson_space/assignment1.py
@@ -0,0 +1,12 @@
+# Assignment 1 -  Write a simple python program that displays a text
+
+def say_hello():
+    print("Hello World") # This is an output statement
+    # Write two more output statements
+
+    name = "Kindson Munonye" # variable assignment
+
+    print("My name is {name}") # String interpolation by using f before the string
+
+if __name__ == "__main__":
+    say_hello()
