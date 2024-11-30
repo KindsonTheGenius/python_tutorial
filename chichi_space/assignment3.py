11592ec5caa447903defcfd6f1719d7468bcbc0a
Chinaza Ohanyiri <tinalexanaza@gmail.com>
2024-11-30 21:37:54 +0100

    my space

---
+++ chichi_space/assignment3.py
@@ -0,0 +1,12 @@
+#assignment 3
+
+
+number = input("please enter a number: ")
+
+number = int(number)
+
+if number > 10:
+    print("Python conditions and loops are a piece of cake")
+else:
+   for i in range(number):
+        print("i am back to check on my skills!")
