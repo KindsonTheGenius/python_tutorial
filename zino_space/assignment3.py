ec5f33d12ef4bdf0e9e2e7be744d082a2ab45754
SittingCrowd <zino.areh@yahoo.com>
2024-12-01 10:43:55 -0600

    update changes

---
+++ zino_space/assignment3.py
@@ -0,0 +1,10 @@
+# assignment 3
+
+num = int(input("Enter a number: ")) # ask the user to enter a number
+
+# prints a statement for x number of times based on the number entered
+for i in range(num):
+    if num <= 10:
+        print("I am back to check on my skills.")
+    else:
+        print("Python loops and conditions are a piece of cake.")
