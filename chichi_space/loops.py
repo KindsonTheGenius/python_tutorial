253251a8e3168f7508b81a1a52d6ae9b52299a28
Chinaza Ohanyiri <tinalexanaza@gmail.com>
2024-12-06 20:25:44 +0100

    my space

--- chichi_space/loops.py
+++ chichi_space/loops.py
@@ -1,5 +1,13 @@
-#Loops
+#Exercise 2.1
 
+# range (5) prints automatically as range(0,5)
 n = 5
-for i in range(n):
-    print ("welcome to python")
+for i in range(5):
+    print (n)
+
+#console prints output as class 'range' with no value
+for i in range(5)
+    type(range(5))
+
+#Exercise 2.2
+
