87c57983c8e88a26af758e6a9640af257bbcf55c
Chinaza Ohanyiri <tinalexanaza@gmail.com>
2024-12-07 18:02:22 +0100

    my space

--- chichi_space/homework.py
+++ chichi_space/homework.py
@@ -107,11 +107,11 @@ print(c)
 #a - converts the float value and prints out 123.0
 #b - converts the float value and prints out 123.0
 #c - converts the float value and prints out 123.23
-#d - converts the float value and prints out 123
-#e - invalid literal for int() with base 10: '123.23'
-#f - converts the float value and prints out 123
-#g - converts the string value and prints out '12'
-#h - converts the string value and prints out '12.2'
+#d - converts the float value and prints out integer 123
+#e - invalid literal for int() with base 10: console prints string '123.23' as output
+#f - converts the float value and prints out integer 123
+#g - converts the value and prints out string '12'
+#h - converts the value and prints out string '12.2'
 #i - converts the boolean value and prints out 'a' as True
 #j - converts the boolean value and prints out '0' as False
 #k - converts the boolean value and prints out 0.1 as True
--- chichi_space/loops.py
+++ chichi_space/loops.py
@@ -1,13 +1,29 @@
 #Exercise 2.1
 
-# range (5) prints automatically as range(0,5)
+# range (5) prints automatically as range from(0,5)
 n = 5
 for i in range(5):
-    print (n)
+    print (i)
 
 #console prints output as class 'range' with no value
-for i in range(5)
+for i in range(5):
     type(range(5))
 
 #Exercise 2.2
+
+#console prints numbers 0-100
+for i in range(100):
+    print (i)
+
+#console prints an arithmetic progression of the number 7
+for i in range(100):
+    if i % 7 == 0:
+        print (i)
+
+#console prints out multiples of only 5
+for i in range(100):
+    if i % 5 == 0 and i % 3 != 0:
+        print(i)
+
+
 
