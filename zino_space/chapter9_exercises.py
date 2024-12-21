84c2a3309e163ee05d9b364303458e074dae7fd7
SittingCrowd <zino.areh@yahoo.com>
2024-12-21 09:06:56 -0600

    update changes

---
+++ zino_space/chapter9_exercises.py
@@ -0,0 +1,53 @@
+# Question 1
+fruits = {"apple","banana","cherry"}
+print(fruits)
+
+# Question 2
+numbers = {1, 2, 3, 4, 5}
+if 3 in numbers:
+    print("Found")
+else:
+    print("Not Found")
+
+# Question 3
+colors ={"red", "green", "blue"}
+colors.add("yellow")
+print(colors)
+
+# Question 4
+animals = {"dog", "cat", "rabbit", "parrot"}
+animals.remove("rabbit")
+print(animals)
+
+# Question 5
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+print(set1 | set2)
+
+# Question 6
+setA = {"apple", "banana", "cherry"}
+setB = {"banana", "cherry", "date"}
+print(setA & setB)
+
+# Question 7
+setX = {10, 20, 30, 40}
+setY = {30, 40, 50, 60}
+print(setX - setY)
+
+# Question 8
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+print(A ^ B)
+
+# Question 9
+num = [1, 2, 2, 3, 4, 4, 5]
+print(set(num))
+
+# Question 10
+small_set = {1, 2, 3}
+large_set = {1, 2, 3, 4, 5}
+print(small_set <= large_set)
+
+# Question Bonus Challenge
+programming_set = {"p", "r", "o", "g", "r", "a", "m", "m", "i", "n", "g"}
+print(programming_set)
