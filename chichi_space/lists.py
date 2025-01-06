3373f546fa0107854cd1826a0ba0e35667a3b2d3
Chinaza Ohanyiri <tinalexanaza@gmail.com>
2025-01-06 16:03:22 +0100

    my space

---
+++ chichi_space/lists.py
@@ -0,0 +1,15 @@
+#Lists in Python
+
+months = ['January', 'February',
+          'March', 'April',
+          'May', 'June',
+          'July', 'August',
+          'September', 'October', 'November'
+          'December']
+
+scores = [45, 5, 40, 22, 3]
+
+more_scores = [1, 2]
+
+item = scores.pop()
+print(item)
